8cbd9aec4311856c2e8c7a4ef0100124b214ab36
obf1313 <chenna2@sisyphe.com>
2022-06-15 14:31:36 +0800

    补充注释

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// https://juejin.cn/post/6844903943902855176
 const React = require('react');
 const { renderToString } = require('react-dom/server');
 const express = require('express');
@@ -11,6 +12,8 @@ const Index = (props) => {
   );
 }
 
+// 2. 获取数据的方法和逻辑些在哪里？
+// 但该方法和组件没有任何关联，我们更希望的是每个路由（？）都有自己的 fetch 方法。
 // 模拟数据获取
 const fetch = () => {
   return {
@@ -19,6 +22,8 @@ const fetch = () => {
   };
 }
 
+// 1. 双端路由如何维护？
+// 定义了路由 /，但在 SPA 模式下我们需要使用 react-router 定义路由。
 app.get('/', (req, res) => {
   const data = fetch();
   res.send(renderToString(<Index data={data} />))
@@ -27,3 +32,10 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('http://localhost:3000');
 });
+
+// 3. 服务端 html 节点无法重用
+// 虽然组件在服务端得到了数据，也能渲染到浏览器内，但是当浏览器端进行组件渲染的时候直出的内容会一闪而过消失。
+
+// 引出同构，ssr 的核心。
+// 所谓同构就是采用一套代码，构建双端（server 和 client）逻辑，最大限度的重用代码，不用维护两套代码。
+// 而传统的服务端渲染是无法做到的，react 的出现打破了这个瓶颈，并且现在已经得到了比较广泛的应用。
