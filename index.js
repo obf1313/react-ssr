cb0ce6f6c56beeff75cc2277284d27f9fa350421
obf1313 <oubaifan@163.com>
2022-06-17 13:48:32 +0800

    数据同构（预取同构）

--- index.js
+++ index.js
@@ -4,26 +4,15 @@
  */
 // https://juejin.cn/post/6844903943902855176
 import { renderToString } from 'react-dom/server';
-import express from 'express';
 import http from 'http';
 import React from 'react';
 import { matchRoutes } from 'react-router-config';
 import routes from './routes-config';
 
-// 2. 获取数据的方法和逻辑些在哪里？
-// 但该方法和组件没有任何关联，我们更希望的是每个路由（？）都有自己的 fetch 方法。
-// 模拟数据获取
-const fetch = () => {
-  return {
-    title: 'react ssr',
-    data: []
-  };
-}
-
 // 1. 双端路由如何维护？
 // 首先我们会发现我在 server 端定义了路由 '/'，但是在 react SPA 模式下我们需要使用 react-router 来定义路由。那是不是就需要维护两套路由呢？
 // 定义了路由 /，但在 SPA 模式下我们需要使用 react-router 定义路由。
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
   const url = req.url;
   // 简单容错，排除图片等资源文件的请求
   if (url.indexOf('.') !== -1) {
@@ -33,11 +22,12 @@ http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text.html'
   });
-  const data = fetch();
   // 查找组件
   const branch = matchRoutes(routes, url);
   // 得到组件
   const Component = branch[0].route.component;
+  // 数据预取
+  const data = await Component.getInitialProps(branch[0].match.params);
   // 将组件渲染为 html 字符串
   const html = renderToString(<Component data={data} />);
   res.end(html);
--- routes-config.js
+++ routes-config.js
@@ -3,20 +3,45 @@
  * @author obf1313
  */
 import React from 'react';
-const Detail = () => {
+
+// 2. 获取数据的方法和逻辑些在哪里？（数据预取同构）
+// 但该方法和组件没有任何关联，我们更希望的是每个路由（？）都有自己的 fetch 方法。
+// 模拟数据获取
+const fetch = (title) => {
+  return {
+    title: title,
+    data: []
+  };
+}
+
+// 主页
+const Home = (props) => {
   return (
     <div>
-      detail
+      {props.data.title}
     </div>
   );
 }
-const Home = () => {
+// 数据预取方法
+Home.getInitialProps = async (opt) => {
+  const fetch1 = await fetch('Home1');
+  return fetch1;
+};
+
+// 详情页
+const Detail = (props) => {
   return (
     <div>
-      home
+      {props.data.title}
     </div>
   );
 }
+// 数据预取方法
+Detail.getInitialProps = async (opt) => {
+  const fetch1 = await fetch('Detail');
+  return fetch1;
+};
+
 const routes = [{
   path: '/',
   exact: true,
